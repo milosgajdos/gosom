af7853f6b24a3cd74c6c179a3a2f1dc17294d420
Milos Gajdos <milosgajdos83@gmail.com>
2016-10-06 00:24:02 +0100

    Added vector distance. Made DistanceMx more efficient. Name changes
    This commit reorganizes distance.go source file a bit. It introduces
    Distance function that calculates a distance betweent two vectors
    based on requested metric. It renames DistanceMatrix to DistanceMx to
    follow naming new conventions. It makes euclideanMx more efficient by
    halfing the number of iterations performed. Finally, NewMap is now
    complete and we can proceed further.

--- som/distance.go
+++ som/distance.go
@@ -1,38 +1,80 @@
 package som
 
 import (
+	"fmt"
 	"math"
 
 	"github.com/gonum/matrix/mat64"
 )
 
-// DistanceMatrix calculates a distance matrix for the given matrix using the given method.  The returned distance
-// matrix is a hollow symmetrical matrix where an item x_ij contains the distance between rows i and j.
-func DistanceMatrix(method string, matrix *mat64.Dense) *mat64.Dense {
-	switch method {
+// Distance calculates a distance metric between vectors a and b.
+// If unsupported metric is requested DistVector returns euclidean ddistance.
+// It returns error if the supplied vectors are either nil are different dimension
+func Distance(metric string, a, b *mat64.Vector) (float64, error) {
+	switch metric {
 	case "euclidean":
-		return euclidean(matrix)
+		return euclideanVec(a, b)
 	default:
-		return euclidean(matrix)
+		return euclideanVec(a, b)
 	}
 }
 
-func euclidean(in *mat64.Dense) *mat64.Dense {
-	rows, cols := in.Dims()
+// DistanceMx calculates a distance matrix for the given matrix using the given method.
+// It returns a hollow symmetric matrix where an item x_ij contains the distance between
+// vectors stored in rows i and j. You can request different distance metrics via metric
+// parameter. If an unknown metric is supplied Euclidean distance is computed.
+func DistanceMx(metric string, matrix *mat64.Dense) (*mat64.Dense, error) {
+	switch metric {
+	case "euclidean":
+		return euclideanMx(matrix)
+	default:
+		return euclideanMx(matrix)
+	}
+}
+
+// euclideanVec computes euclidean distances between vectors a and b.
+// It returns error if the supplied vectors are either nil are different dimension
+func euclideanVec(a, b *mat64.Vector) (float64, error) {
+	if a == nil || b == nil {
+		return 0.0, fmt.Errorf("Invalid vectors supplied. a: %v, b: %v\n", a, b)
+	}
+	if a.Len() != b.Len() {
+		return 0.0, fmt.Errorf("Incorrect vector dims. a: %d, b: %d\n", a.Len(), b.Len())
+	}
+
+	d := new(mat64.Dense)
+	d.Scale(-1.0, b)
+	d.Add(a, d)
+
+	// power function to be applied to each matrix element
+	powFn := func(i, j int, x float64) float64 {
+		return math.Pow(x, 2.0)
+	}
+	d.Apply(powFn, d)
+
+	return math.Sqrt(mat64.Sum(d)), nil
+}
+
+func euclideanMx(in *mat64.Dense) (*mat64.Dense, error) {
+	if in == nil {
+		return nil, fmt.Errorf("Invalid matrix supplied: %v\n", in)
+	}
+
+	rows, _ := in.Dims()
 	out := mat64.NewDense(rows, rows, nil)
 
-	for row := 0; row < rows; row++ {
-		for i := 0; i < rows; i++ {
-			dist := 0.0
+	for row := 0; row < rows-1; row++ {
+		dist := 0.0
+		a := in.RowView(row)
+		for i := row + 1; i < rows; i++ {
 			if i != row {
-				for j := 0; j < cols; j++ {
-					dist += math.Pow(in.At(row, j)-in.At(i, j), 2.0)
-				}
-				dist = math.Sqrt(dist)
+				b := in.RowView(i)
+				dist, _ = euclideanVec(a, b)
+				out.Set(row, i, dist)
+				out.Set(i, row, dist)
 			}
-			out.Set(row, i, dist)
 		}
 	}
 
-	return out
+	return out, nil
 }
