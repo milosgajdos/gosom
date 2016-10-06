819c6219f4735f91958b375a7d1968157c86aeea
Eduard Semsch <eduard.semsch@seznam.cz>
2016-10-06 12:29:37 +0000

    Added Mexican hat neighborhood function as well

--- som/neighb.go
+++ som/neighb.go
@@ -14,5 +14,5 @@ func Bubble(distance float64, radius float64) float64 {
 
 // Mexican calculates mexican hat neghbourhood
 func Mexican(distance float64, radius float64) float64 {
-	return 0.0
+	return 2 / (math.Sqrt(3*radius) * math.Pow(math.Pi, 0.25)) * (1 - (distance*distance)/(radius*radius)) * math.Exp(-(distance*distance)/(2*radius*radius))
 }
