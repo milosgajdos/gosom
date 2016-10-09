731e3c3deb636f4f514a9988efe2c2faeb0b2d77
Eduard Semsch <eduard.semsch@seznam.cz>
2016-10-09 20:43:58 +0000

    improved test coverage

--- som/radius_test.go
+++ som/radius_test.go
@@ -7,23 +7,24 @@ import (
 )
 
 func TestExpRadius(t *testing.T) {
-	assert := assert.New(t)
-
-	radius0 := 100.0
-	totalIterations := 100
-	strategy := "exp"
-
-	assert.Equal(radius0, Radius(0, totalIterations, strategy, radius0))
-	assert.InDelta(1.0, Radius(totalIterations, totalIterations, strategy, radius0), 0.01)
+	testRadius(t, "exp")
 }
 
 func TestLinRadius(t *testing.T) {
+	testRadius(t, "lin")
+}
+
+func TestDefaultRadius(t *testing.T) {
+	testRadius(t, "some other")
+}
+
+func testRadius(t *testing.T, strategy string) {
 	assert := assert.New(t)
 
 	radius0 := 100.0
 	totalIterations := 100
-	strategy := "lin"
 
 	assert.Equal(radius0, Radius(0, totalIterations, strategy, radius0))
 	assert.InDelta(1.0, Radius(totalIterations, totalIterations, strategy, radius0), 0.01)
+
 }
