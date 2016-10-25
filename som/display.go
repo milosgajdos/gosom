4b58cd1602be64dc61501e5e40e77ab9ed60c3db
Eduard Semsch <eduard.semsch@seznam.cz>
2016-10-25 16:00:31 +0000

    batch algo - potential fix

--- som/display.go
+++ som/display.go
@@ -29,15 +29,27 @@ type svgElement struct {
 	XMLName  xml.Name `xml:"svg"`
 	Width    float64  `xml:"width,attr"`
 	Height   float64  `xml:"height,attr"`
-	Polygons []polygon
+	Polygons []interface{}
 }
 
-// UMatrixSVG creates SVG representation of the U-Matrix of the given codebook.
-// codebook is the SOM codebook we're rendering the U-Matrix for,
-// coordsDims are the dimensions of the grid, uShape is the shape of the grid unit,
-// title is the title of the output SVG, and writer is the io.Writter to write the output SVG to.
-// UMatrixSVG returns error when the SVG could not be generated.
-func UMatrixSVG(codebook *mat64.Dense, coordsDims []int, uShape string, title string, writer io.Writer) error {
+type textElement struct {
+	XMLName xml.Name `xml:"text"`
+	X       float64  `xml:"x,attr"`
+	Y       float64  `xml:"y,attr"`
+	Text    string   `xml:",innerxml"`
+}
+
+//var colors = [][]int{{255, 255, 255}}
+
+var colors = [][]int{{255, 0, 0}, {0, 255, 0}, {0, 0, 255}, {255, 255, 0}, {255, 0, 255}, {0, 255, 255}}
+
+// Creates an SVG representation of the U-Matrix of the given codebook.
+// codebook is the codebook we're displaying the U-Matrix for,
+// coordsDims are the dimensions of the grid,
+// uShape is the shape of the grid,
+// title is the title of the output SVG,
+// and writer is the io.Writter to write the output SVG to.
+func UMatrixSVG(codebook *mat64.Dense, coordsDims []int, uShape string, title string, writer io.Writer, clusters map[int]int) error {
 	xmlEncoder := xml.NewEncoder(writer)
 	// array to hold the xml elements
 	elems := []interface{}{h1{Title: title}}
@@ -61,14 +73,14 @@ func UMatrixSVG(codebook *mat64.Dense, coordsDims []int, uShape string, title st
 	maxDistance := mat64.Max(distMat)
 
 	// function to scale the coord grid to something visible
-	const MUL = 20.0
+	const MUL = 50.0
 	const OFF = 10.0
 	scale := func(x float64) float64 { return MUL*x + OFF }
 
 	svgElem := svgElement{
 		Width:    float64(coordsDims[1])*MUL + 2*OFF,
 		Height:   float64(coordsDims[0])*MUL + 2*OFF,
-		Polygons: make([]polygon, rows),
+		Polygons: make([]interface{}, rows*2),
 	}
 	elems = append(elems, svgElem)
 	for row := 0; row < rows; row++ {
@@ -89,7 +101,16 @@ func UMatrixSVG(codebook *mat64.Dense, coordsDims []int, uShape string, title st
 			}
 		}
 		avgDistance /= float64(len(allRowsInRadius) - 1)
-		color := int((1.0 - avgDistance/maxDistance) * 255.0)
+		var colorMask []int
+		if clusters[row] == -1 {
+			colorMask = []int{255, 255, 255}
+		} else {
+			colorMask = colors[clusters[row]%len(colors)]
+		}
+		colorMul := (1.0 - avgDistance/maxDistance)
+		r := int(colorMul * float64(colorMask[0]))
+		g := int(colorMul * float64(colorMask[1]))
+		b := int(colorMul * float64(colorMask[2]))
 		polygonCoords := ""
 		x := scale(coord.At(0, 0))
 		y := scale(coord.At(1, 0))
@@ -106,9 +127,15 @@ func UMatrixSVG(codebook *mat64.Dense, coordsDims []int, uShape string, title st
 		polygonCoords += fmt.Sprintf("%f,%f ", x-xOffset, y+yOffset)
 		polygonCoords += fmt.Sprintf("%f,%f ", x+xOffset, y+yOffset)
 
-		svgElem.Polygons[row] = polygon{
+		svgElem.Polygons[row*2] = polygon{
 			Points: []byte(polygonCoords),
-			Style:  fmt.Sprintf("fill:rgb(%d,%d,%d);stroke:black;stroke-width:1", color, color, color),
+			Style:  fmt.Sprintf("fill:rgb(%d,%d,%d);stroke:black;stroke-width:1", r, g, b),
+		}
+
+		svgElem.Polygons[row*2+1] = textElement{
+			X:    x - 0.25*MUL,
+			Y:    y + 0.25*MUL,
+			Text: fmt.Sprintf("%d", clusters[row]),
 		}
 	}
 
--- som/som.go
+++ som/som.go
@@ -12,64 +12,42 @@ import (
 	"github.com/milosgajdos83/gosom/pkg/utils"
 )
 
-// CodebookInitFunc defines SOM codebook initialization function
 type CodebookInitFunc func(*mat64.Dense, []int) (*mat64.Dense, error)
 
-// CoordsInitFunc defines SOM grid coordinates initialization function
 type CoordsInitFunc func(string, []int) (*mat64.Dense, error)
 
-// NeighbFunc defines SOM neighbourhood function
 type NeighbFunc func(float64, float64) float64
 
-// Map is a Self Organizing Map (SOM)
 type Map struct {
-	// codebook is a matrix which contains SOM codebook vectors
-	// codebook dimensions: SOM units x data features
 	codebook *mat64.Dense
-	// unitDist is a symmetric hollow matrix that maps distances between SOM units
-	// unitDist dimesions: SOM units x SOM units
 	unitDist *mat64.Dense
-	// bmus stores codebook row indices of Best Match Units (BMU) over the training
-	// bmus will give us an indication of how many clusters are there in the data
-	bmus map[int]int
+	bmus     map[int]int
 }
 
-// NewMap creates new SOM based on the provided configuration and input data
-// NewMap allows you to pass in SOM codebook init function that is used to initialize
-// SOM codebook vectors to initial values. If codebook InitFunc is nil, random initialization
-// is used. NewMap returns error if the provided configuration is not valid or if the data matrix
-// is nil or if the codebook matrix could not be initialized.
 func NewMap(c *MapConfig, data *mat64.Dense) (*Map, error) {
-	// if input data is empty throw error
 	if data == nil {
 		return nil, fmt.Errorf("Invalid input data: %v\n", data)
 	}
-	// validate the map configuration
 	if err := validateMapConfig(c); err != nil {
 		return nil, err
 	}
-	// compute the number of map units
 	mUnits := utils.IntProduct(c.Dims)
 	if mUnits <= 1 {
 		return nil, fmt.Errorf("Incorrect map size dimensions: %v\n", c.Dims)
 	}
-	// initialize codebook
 	codebook, err := c.InitFunc(data, c.Dims)
 	if err != nil {
 		return nil, err
 	}
-	// grid coordinates matrix
 	gridCoords, err := GridCoords(c.UShape, c.Dims)
 	if err != nil {
 		return nil, err
 	}
-	// unit distance matrix
 	unitDist, err := DistanceMx("euclidean", gridCoords)
 	if err != nil {
 		return nil, err
 	}
 	bmus := make(map[int]int)
-	// return pointer to new map
 	return &Map{
 		codebook: codebook,
 		unitDist: unitDist,
@@ -77,57 +55,40 @@ func NewMap(c *MapConfig, data *mat64.Dense) (*Map, error) {
 	}, nil
 }
 
-// Codebook returns a matrix which contains SOM codebook vectors
 func (m Map) Codebook() *mat64.Dense {
 	return m.codebook
 }
 
-// UnitDist returns a matrix which contains Euclidean distances between SOM units
 func (m Map) UnitDist() *mat64.Dense {
 	return m.unitDist
 }
 
-// BMUs returns a slice which contains indices of Best Match Units (BMUs) of each input vector
 func (m Map) BMUs() map[int]int {
 	return m.bmus
 }
 
-// MarshalTo serializes SOM codebook in a given format to writer w.
-// At the moment only the native gonum binary format is supported.
-// It returns the number of bytes written to w or fails with error.
 func (m *Map) MarshalTo(format string, w io.Writer) (int, error) {
 	switch format {
 	case "gonum":
 		return m.codebook.MarshalBinaryTo(w)
 	}
-	// marshal binary to file path
 	return 0, fmt.Errorf("Unsupported format: %s\n", format)
 }
 
-// UMatrixOut generates SOM u-matrix in a given format and writes the output to w.
-// At the moment only SVG format is supported. It fails with error if the write to w fails.
 func (m Map) UMatrixOut(format, title string, w io.Writer) error {
-	// TODO: needs some UMatrixSVG modifications first
 	return nil
 }
 
-// Train runs a SOM training for a given data set and training configuration parameters.
-// It modifies the map codebook vectors based on the chosen training algorithm.
-// It returns error if the supplied training configuration is invalid or training fails
 func (m *Map) Train(c *TrainConfig, data *mat64.Dense, iters int) error {
-	// number of iterations must be a positive integer
 	if iters <= 0 {
 		return fmt.Errorf("Invalid number of iterations: %d\n", iters)
 	}
-	// nil data passed in
 	if data == nil {
 		return fmt.Errorf("Invalid data supplied: %v\n", data)
 	}
-	// validate the training configuration
 	if err := validateTrainConfig(c); err != nil {
 		return err
 	}
-	// run the training
 	switch c.Method {
 	case "seq":
 		return m.seqTrain(c, data, iters)
@@ -138,34 +99,20 @@ func (m *Map) Train(c *TrainConfig, data *mat64.Dense, iters int) error {
 	return nil
 }
 
-// seqTrain runs sequential SOM training algorithm on a given data set
 func (m *Map) seqTrain(tc *TrainConfig, data *mat64.Dense, iters int) error {
 	rows, _ := data.Dims()
-	// create random number generator
 	rSrc := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(rSrc)
-	// retrieve Neighbourhood function
 	neighbFn := Neighb[tc.NeighbFn]
-	// perform iters number of learning iterations
 	for i := 0; i < iters; i++ {
-		// pick a random sample from dataset
 		sample := data.RowView(r.Intn(rows))
-		// no need to check for error here:
-		// sample and codebook are not nil and have the same dimension
 		bmu, _ := ClosestVec("euclidean", sample, m.codebook)
-		// no need to check for errors:
-		// LRate and Radius are checked by config validation
 		lRate, _ := LRate(i, iters, tc.LDecay, tc.LRate)
 		radius, _ := Radius(i, iters, tc.RDecay, tc.Radius)
-		// pick the bmu unit distance row
 		bmuDists := m.unitDist.RowView(bmu)
-		// find units which are within the radius
 		for i := 0; i < bmuDists.Len(); i++ {
-			// bmu distance to i-th map unit
 			dist := bmuDists.At(i, 0)
-			// we are within BMU radius
 			if dist < radius {
-				// update particular codebook vector
 				m.seqUpdateCbVec(i, sample, lRate, radius, dist, neighbFn)
 			}
 		}
@@ -174,109 +121,83 @@ func (m *Map) seqTrain(tc *TrainConfig, data *mat64.Dense, iters int) error {
 	return nil
 }
 
-// seqUpdateCbVec updates codebook vector on row cbIdx given the learning rate l,
-// radius r, distance d and neihgbourhood function nFn
 func (m *Map) seqUpdateCbVec(cbIdx int, vec *mat64.Vector, l, r, d float64, nFn NeighbFunc) {
-	// pick codebook vector that should be updated
 	cbVec := m.codebook.RowView(cbIdx)
-	// update codebook vector according to the algorithm
 	diff := mat64.NewVector(cbVec.Len(), nil)
 	diff.AddScaledVec(vec, -1.0, cbVec)
 	mul := l
-	// nFn returns 1 for d == 0; skipping this case will save us some CPU time
 	if d > 0.0 {
 		mul *= nFn(d, r)
 	}
 	cbVec.AddScaledVec(cbVec, mul, diff)
 }
 
-// batchConfig holds batch training configuration
 type batchConfig struct {
-	// tc is a training configuration
-	tc *TrainConfig
-	// iters is a number of batch iterations
+	tc    *TrainConfig
 	iters int
 }
 
-// batchResult holds scaled data vector, neighbourhood of data input and index of codebook vector
 type batchResult struct {
-	// vec is a scaled data vector
-	vec *mat64.Vector
-	// nghb is vec BMU neighbourhood
+	vec  *mat64.Vector
 	nghb float64
-	// idx is an index of codebook vector to update
-	idx int
+	idx  int
 }
 
-// batchTrain runs batch SOM training on a given data set
 func (m *Map) batchTrain(tc *TrainConfig, data *mat64.Dense, iters int) error {
 	cbRows, _ := m.codebook.Dims()
 	rows, _ := data.Dims()
-	// bSize set to min(cbRows,rows)
 	bSize := cbRows
 	if rows < cbRows {
 		bSize = rows
 	}
-	// number of batchIters in one learning iteration
 	bIters := rows / bSize
 	if rows%bSize != 0 {
 		bIters++
 	}
 	fmt.Println("batch iters", bIters)
-	// batchConfig: training config and number of iterations
 	bc := &batchConfig{
 		tc:    tc,
 		iters: bIters,
 	}
-	// number of worker goroutines
 	workers := runtime.NumCPU()
-	// train for a number of iterations
 	for i := 0; i < iters; i++ {
 		count := bSize
-		iter := 1
-		// create batch results channel
-		results := make(chan *batchResult, workers*4)
-		wg := &sync.WaitGroup{}
-		// Iterate over the input data in batchSize * workers
-		for j := 0; j < rows; j += bSize * workers {
-			// start worker goroutines
+		iter := 0
+		for j := 0; j < rows; j += bSize {
+			results := make(chan *batchResult, workers*4)
+			wg := &sync.WaitGroup{}
+			// Changes here: bSize/workers etc.
 			for k := 0; k < workers; k++ {
-				// if we go over the number of rows adjust bSamples
-				from := j + k*bSize
+				from := j + k*(bSize/workers)
 				if from+bSize > rows {
 					count = rows - from
 				}
-				//fmt.Println("FROM", from, "COUNT", count)
 				wg.Add(1)
 				go m.processBatch(results, wg, bc, data, from, count, iter)
-				// no need to spin another goroutine
 				if count < bSize {
 					break
 				}
-				iter++
 			}
-		}
-		// wait for workers to finish and close the result channel
-		go func() {
-			wg.Wait()
-			close(results)
-		}()
-		// collect batch results from all workers
-		cbVecs := make([]*mat64.Vector, cbRows)
-		nghbs := make([]float64, cbRows)
-		for result := range results {
-			if cbVecs[result.idx] != nil {
-				cbVecs[result.idx].AddVec(cbVecs[result.idx], result.vec)
-			} else {
-				cbVecs[result.idx] = result.vec
+			iter++
+			go func() {
+				wg.Wait()
+				close(results)
+			}()
+			cbVecs := make([]*mat64.Vector, cbRows)
+			nghbs := make([]float64, cbRows)
+			for result := range results {
+				if cbVecs[result.idx] != nil {
+					cbVecs[result.idx].AddVec(cbVecs[result.idx], result.vec)
+				} else {
+					cbVecs[result.idx] = result.vec
+				}
+				nghbs[result.idx] += result.nghb
 			}
-			nghbs[result.idx] += result.nghb
-		}
-		// update codebook vectors
-		for k := 0; k < cbRows; k++ {
-			if cbVecs[k] != nil {
-				cbVecs[k].ScaleVec(1.0/nghbs[k], cbVecs[k])
-				m.codebook.SetRow(k, cbVecs[k].RawVector().Data)
+			for k := 0; k < cbRows; k++ {
+				if cbVecs[k] != nil {
+					cbVecs[k].ScaleVec(1.0/nghbs[k], cbVecs[k])
+					m.codebook.SetRow(k, cbVecs[k].RawVector().Data)
+				}
 			}
 		}
 	}
@@ -284,37 +205,23 @@ func (m *Map) batchTrain(tc *TrainConfig, data *mat64.Dense, iters int) error {
 	return nil
 }
 
-// processRow processes data rows and sends tehm down the results channel
 func (m Map) processBatch(res chan<- *batchResult, wg *sync.WaitGroup,
 	bc *batchConfig, data *mat64.Dense, from, count, iter int) {
-	//fmt.Println("batch FROM", from, "batch COUNT", count)
 	fmt.Println("ITER", iter)
-	// retrieve matrix dimension
 	_, cols := data.Dims()
-	// retrieve Neighbourhood function
 	neighbFn := Neighb[bc.tc.NeighbFn]
 	for i := from; i < count+from; i++ {
-		//fmt.Println("Row", i)
 		row := data.RowView(i)
-		// find codebook BMU for this data row
 		bmu, _ := ClosestVec("euclidean", row, m.codebook)
-		// calculate radius for this iteration
 		radius, _ := Radius(iter, bc.iters, bc.tc.RDecay, bc.tc.Radius)
-		fmt.Println("RADIUS", radius)
-		// pick the BMU's distance row
 		bmuDists := m.unitDist.RowView(bmu)
 		for j := 0; j < bmuDists.Len(); j++ {
-			// bmu distance to i-th map unit
 			dist := bmuDists.At(j, 0)
-			// when in BMU radius, scale and add to all neighbourhood vecs
 			if dist < radius {
-				// calculate neighbourhood function
 				nghb := neighbFn(dist, radius)
-				// allocate new vector and copy row data to it
 				vec := mat64.NewVector(cols, nil)
 				vec.CopyVec(row)
 				vec.ScaleVec(nghb, vec)
-				// send batchResult down results channel
 				res <- &batchResult{vec: vec, nghb: nghb, idx: j}
 			}
 		}
