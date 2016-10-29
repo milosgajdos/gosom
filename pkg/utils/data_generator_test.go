00a2aa3248eaa3f78a1463746815fe3ddc4f7172
Eduard Semsch <eduard.semsch@seznam.cz>
2016-10-29 13:22:43 +0000

    Added data generation to the utils package

---
+++ pkg/utils/data_generator_test.go
@@ -0,0 +1,69 @@
+package utils
+
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInvariants(t *testing.T) {
+	assert := assert.New(t)
+
+	const rows = 20
+	const columns = 2
+	const numOfClusters = 10
+	const min = 0.0
+	const max = 1.0
+	const vari = 0.1
+	const randSeed = 1
+	data := CreateClusteredData(20, 2, 10, 0.0, 1.0, 0.1, 1)
+
+	dRows, dCols := data.Dims()
+	assert.Equal(rows, dRows)
+	assert.Equal(columns, dCols)
+	for i := 0; i < dRows; i++ {
+		for j := 0; j < dCols; j++ {
+			val := data.At(i, j)
+			assert.True(val >= min-vari)
+			assert.True(val <= max+vari)
+		}
+	}
+}
+
+func Test0Var(t *testing.T) {
+	assert := assert.New(t)
+
+	data := CreateClusteredData(100, 2, 1, 0.0, 1.0, 0.0, 1)
+
+	dRows, dCols := data.Dims()
+	prevVal := make([]*float64, dCols)
+	for i := 0; i < dRows; i++ {
+		for j := 0; j < dCols; j++ {
+			val := data.At(i, j)
+			if prevVal[j] != nil {
+				assert.Equal(*prevVal[j], val)
+			}
+			prevVal[j] = &val
+		}
+	}
+}
+
+func TestWithVar(t *testing.T) {
+	assert := assert.New(t)
+
+	const vari = 0.1
+	data := CreateClusteredData(100, 2, 1, 0.0, 1.0, vari, 1)
+
+	dRows, dCols := data.Dims()
+	prevVal := make([]*float64, dCols)
+	for i := 0; i < dRows; i++ {
+		for j := 0; j < dCols; j++ {
+			val := data.At(i, j)
+			if prevVal[j] != nil {
+				assert.True(math.Abs(*prevVal[j]-val) < 2*vari)
+			}
+			prevVal[j] = &val
+		}
+	}
+}
